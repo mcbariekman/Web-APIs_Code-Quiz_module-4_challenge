a9aeb3cad15a9c73d6756ab038f4e4a2ed985b3d
MC <119978958+mcbariekman@users.noreply.github.com>
2023-01-26 21:41:59 -0800

    fixing code

---
+++ assets/questions.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question : "What is the name of this channel",
+        options : [
+            "Code Sculptor",
+            "Code Builder",
+            "Code Destroyer",
+            "Code Maker"
+        ],
+        answer : "Code Sculptor"
+    },
+    {
+        question : "1 + 1 is?",
+        options : [
+            "2", "3", "5", "19"
+        ],
+        answer : 2
+    },
+    {
+        question : "Which of this is a programming language?",
+        options : [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "JSON"
+        ],
+        answer : "Javascript"
+    },
+    {
+        question : "What is my name",
+        options : [
+            "Femi Fatokun",
+            "David Shokunbi",
+            "Wale Adenusi",
+            "Daniel Adewale"
+        ],
+        answer : "Femi Fatokun"
+    }
+]
